81d2fe353189d3f00b0073f26da7e8a50a255616
samsoleh <samsoleh@gmail.com>
2022-08-31 09:33:40 +0700

    commit tambahan data class

---
+++ app/src/main/java/com/example/helloworld/Hewan.kt
@@ -0,0 +1,6 @@
+package com.example.helloworld
+
+data class Hewan(
+    val nama:String,
+    val gambarResId:Int
+)
--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val data = ArrayList<Hewan>().apply{
+            add(Hewan("Ayam",R.drawable.ayam))
+            add(Hewan("Bebek",R.drawable.bebek))
+            add(Hewan("Domba",R.drawable.domba))
+            add(Hewan("Kambing",R.drawable.kambing))
+            add(Hewan("Sapi",R.drawable.sapi))
+        }
+
     }
 }
