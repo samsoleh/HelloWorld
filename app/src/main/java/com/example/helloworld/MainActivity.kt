29cc0abd7b46bd78f6f09b81d6380e283a20a82a
samsoleh <samsoleh@gmail.com>
2022-08-31 10:04:19 +0700

    commit tambahan button

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var textView: TextView
+    private var index = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +23,20 @@ class MainActivity : AppCompatActivity() {
             add(Hewan("Sapi",R.drawable.sapi))
         }
 
+        imageView = findViewById(R.id.imageView2)
+        textView = findViewById(R.id.textView)
+        showData(data[0])
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            index++
+            index = (index+1) % data.size
+            showData(data[index])
+        }
+    }
+
+    private fun showData(hewan: Hewan){
+        imageView.setImageResource(hewan.gambarResId)
+        textView.text = hewan.nama
     }
 }
